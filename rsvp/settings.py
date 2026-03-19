4a28728f5b008443d663f6fa34528910344de547
Mario Michel <mariomichel58@gmail.com>
2026-03-18 20:39:59 -0500

    Move migrate to Procfile release command

--- rsvp/settings.py
+++ rsvp/settings.py
@@ -162,12 +162,12 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 
 """
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = 'seph.n.mario@gmail.com'
 
 """ssl._create_default_https_context = ssl._create_unverified_context """
